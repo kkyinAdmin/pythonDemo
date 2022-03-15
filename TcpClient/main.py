4616ad96f0df6948890b5214df17acdc2a0697a2
kkyinAdmin <yinkaikai828@qq.com>
2022-03-15 17:49:24 +0800

    修改项目名称0000

---
+++ TcpClient/main.py
@@ -0,0 +1,66 @@
+# 这是一个示例 Python 脚本。
+# socket客户端 输入IP 端口号与服务端建立连接 配合demo02调用
+
+import socket
+
+from pip._vendor.distlib.compat import raw_input
+
+
+def socketConnect(argvSocketIP, argvSocketPORT):
+    try:
+        skClient.connect((argvSocketIP, int(argvSocketPORT)))  # 尝试连接服务器
+    except:
+        print("连接异常！")
+        return False
+    else:
+        print("连接成功！")
+        return True
+
+
+def socketSend(argvSendMsg):
+    try:
+        print('DAT--->：' + argvSendMsg)
+        skClient.sendall(argvSendMsg.encode("utf-8"))
+        # ret = skClient.recv(1024)  # 对话(发送/接收)
+        # print(ret.decode())
+    except:
+        print("发送异常！")
+
+
+def socketRecv():
+    try:
+        tmpRecvMsg = skClient.recv(1024)  # 对话(发送/接收)
+        print('DAT<---：' + tmpRecvMsg.decode())
+        if tmpRecvMsg.decode("utf-8") == "":
+            socketClose()
+        return tmpRecvMsg.decode("utf-8")
+    except:
+        print("接收异常！")
+        socketClose()
+        return ""
+
+
+def socketClose():
+    skClient.close()  # 关闭客户套接字
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    global skClient
+    # print("未进行socket连接，请先进行连接！！！")
+    # socketIP = input('请输入IP：')
+    # socketPORT = input('请输入端口：')
+    # socketConnect(socketIP, socketPORT)
+    recvMsg = ""
+    while 1:
+        if recvMsg == "":  # 初始提交和异常接受都为空
+            print("未进行socket连接，请先进行连接！！！")
+            socketIP = input('请输入IP：')
+            socketPORT = input('请输入端口：')
+            skClient = socket.socket()  # 创建客户套接字
+            ret = socketConnect(socketIP.strip(), socketPORT.strip())
+            if not ret:
+                continue
+        sendMsg = raw_input('请输入要发送的报文：')  # input
+        socketSend(sendMsg)
+        recvMsg = socketRecv()
---
+++ TcpServer/main.py
@@ -0,0 +1,35 @@
+# 这是一个示例 Python 脚本。
+# socket服务端 绑定输入的端口 配合demo01调用
+
+import socket
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    global skServer
+    recvMsg = ''
+    while 1:
+        if recvMsg == '':
+            socketPORT = input('请输入绑定端口：')
+            try:
+                skServer = socket.socket()  # 创建客户套接字
+                skServer.bind(('127.0.0.1', int(socketPORT.strip())))  # 把地址绑定到套接字
+                skServer.listen()  # 监听链接
+            except:
+                print('绑定异常！')
+                continue
+            print("端口绑定成功，等待客户端连接")
+            conn, addr = skServer.accept()  # 接受客户端链接
+            print("客户端连接成功")
+        try:
+            tmpRecvMsg = conn.recv(1024)  # 接收客户端信息
+        except:
+            print('获取客户端报文异常！')
+            conn.close()  # 关闭客户端套接字
+            skServer.close()  # 关闭服务器套接字(可选)  代码这里直接关闭服务器套接字重新绑定端口
+            recvMsg = ''
+            continue
+        recvMsg = tmpRecvMsg.decode("utf-8")  # byte解码 如b'hi'转为'hi'
+        print('<---'+recvMsg)
+        sendMsg = "hi"
+        conn.send(sendMsg.encode("utf-8"))
+        print('--->' + sendMsg)
