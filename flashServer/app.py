42b90a4390516ae7db660306700b6af01643717c
kkyinAdmin <yinkaikai828@qq.com>
2022-07-12 12:40:27 +0800

    多个指向

--- flashServer/app.py
+++ flashServer/app.py
@@ -14,9 +14,16 @@ def login():
 
 
 # 转换器
-@app.route('/index/<uid>')
+@app.route('/uid/<uid>')
 def index(uid):
     return "id为：" + uid
+
+
+# 多个指向
+@app.route('/page1')
+@app.route('/page2')
+def page0():
+    return "这是page0"
 
 
 if __name__ == '__main__':
