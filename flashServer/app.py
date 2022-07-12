123dc78295ebf8a412f35b87d4b7c15ce992fdeb
kkyinAdmin <yinkaikai828@qq.com>
2022-07-12 12:35:11 +0800

    转换器

--- flashServer/app.py
+++ flashServer/app.py
@@ -12,6 +12,11 @@ def hello_world():
 def login():
     return '登录成功'
 
+# 转换器
+@app.route('/index/<uid>')
+def index(uid):
+    return "id为：" + uid
+
 
 if __name__ == '__main__':
     app.run()
---
+++ httpClient/main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import http.client
+from urllib import request, parse
+
+
+def httpPost(url, path, data, header):
+    conn = http.client.HTTPConnection(url)
+    conn.request("POST", path, data, header)
+    r1 = conn.getresponse()
+    # print(r1)
+    print(r1.status, r1.reason)
+
+    recvData = r1.read()
+    print(recvData)  #
+    conn.close()
+    return recvData
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    url = "http://127.0.0.1:5000"
+    data = {
+        'test',
+    }
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    recvData = httpPost(url, path, data, headers)
+    # print("收到返回"+recvData)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
